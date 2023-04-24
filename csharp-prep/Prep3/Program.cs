cc95a50cf46057a9d26eed336c1a7b8c4ef855ca
Jada Bower <bow22005@byui.edu>
2023-04-24 12:24:45 -0600

    Magic Number Guesser

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(0, 101);
+
+        int guessNum;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guessAsString = Console.ReadLine();
+            guessNum = int.Parse(guessAsString);
+
+            if (guessNum > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNum < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while(guessNum != magicNum);
     }
 }
