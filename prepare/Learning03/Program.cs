b8d8f15faf4d17dee2ad7b3f19e1b915d255d0b4
Jada Bower <bow22005@byui.edu>
2023-05-17 10:46:42 -0600

    Fraction program

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction frac1 = new Fraction();
+
+        Console.WriteLine(frac1.GetFractionString());
+        Console.WriteLine(frac1.GetDecimalValue());
+
+        Fraction frac2 = new Fraction(5);
+
+        Console.WriteLine(frac2.GetFractionString());
+        Console.WriteLine(frac2.GetDecimalValue());
+
+        Fraction frac3 = new Fraction(6, 7);
+        
+        Console.WriteLine(frac3.GetFractionString());
+        Console.WriteLine(frac3.GetDecimalValue());
+
+        frac3.SetNumerator(3);
+        frac3.SetDenominator(4);
+
+        Console.WriteLine(frac3.GetFractionString());
+        Console.WriteLine(frac3.GetDecimalValue());
     }
 }
