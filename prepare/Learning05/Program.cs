8f85708157df7798c171bc91b8f468bdda2c4d88
Jada Bower <bow22005@byui.edu>
2023-06-14 14:10:04 -0600

    polymorphism prep program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square(4.0, "red");
+        Rectangle rect = new Rectangle(2.0, 4.2, "blue");
+        Circle cir = new Circle(3.14, "green");
+        
+        shapes.Add(square);
+        shapes.Add(rect);
+        shapes.Add(cir);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
